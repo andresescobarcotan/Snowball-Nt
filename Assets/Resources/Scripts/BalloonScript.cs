d319674e0d28d56a46cf8f5b28dff4274d4f6751
Andrés Escobar Cotán <andres.escobar.cotan@gmail.com>
2024-12-06 19:32:26 +0100

    fix: Bug with player
    feat: Xploding baloons

--- Assets/Resources/Scripts/BalloonScript.cs
+++ Assets/Resources/Scripts/BalloonScript.cs
@@ -1,22 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class BalloonScripy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float maxHeight = 2.0f;
+    public float velocity = 1.0f;
+    float startHeight =0;
+
+    public GameObject particles;
+
     void Start()
     {
-        
+        startHeight = transform.position.y;
+        maxHeight = maxHeight - startHeight;
+        velocity -= Random.Range(-0.5f,0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 temp = transform.position;
+        temp.y -= velocity * Time.deltaTime;
+        if (temp.y < startHeight || temp.y > maxHeight)
+        {
+            velocity *= -1;
+        }
+        transform.position = temp;
     }
 
     private void OnCollisionEnter(Collision collision) {
+        GameObject clone = Instantiate(particles, transform.position, transform.rotation);
+        Destroy(clone.gameObject, 2);
         Destroy(gameObject);
         Destroy(collision.gameObject);
     }
